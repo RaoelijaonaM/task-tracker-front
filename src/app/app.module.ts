2fa3d1c5a712ae97618ed8d981abecf49b4304a9
Raoelijaona <mitiaraoelijaona1@gmail.com>
2022-09-06 14:49:01 +0300

    designing card space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+/***********************Module************************/
+import { RouterModule, Routes } from '@angular/router';
+/*************Component*****************************/
 import { AppComponent } from './app.component';
+import { CardComponent } from './espace/card/card.component';
+import { TaskComponent } from './espace/card/task/task.component';
+import { EspaceComponent } from './espace/espace.component';
 
+const routes: Routes = [{ path: 'espace', component: EspaceComponent }];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EspaceComponent, CardComponent, TaskComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
